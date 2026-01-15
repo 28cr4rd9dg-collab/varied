463d8bde0e830296f959db76ae55bff7b136ad5e
Manu <28cr4rd9dg@privaterelay.appleid.com>
2026-01-15 09:00:58 +0100

    Add files via upload
    RGB image

---
+++ RGB.py
@@ -0,0 +1,17 @@
+from turtle import *
+from colorsys import hsv_to_rgb
+
+bgcolor = 'black'
+speed(0)
+h = 0
+
+for i in range(200):
+    h += 0.0015
+    color(hsv_to_rgb(h, 1, 1))
+    goto(0, 0)
+    fd(i)
+    circle(200, 50)
+    rt(10)
+
+hideturtle()
+done()
