afabe860a6d7f103bc3490a7209ca7361158669f
Manu <28cr4rd9dg@privaterelay.appleid.com>
2026-01-15 09:03:05 +0100

    Add files via upload
    weather IF ELIF

---
+++ Weather.py
@@ -0,0 +1,9 @@
+is_hot = False
+is_cold = True
+if is_hot:
+    print("it's a hot day")
+    print("drink plnty of water")
+elif is_cold:
+    print("it's a cold day")
+    print("wear warm clothes")
+print("enjoy your day")
